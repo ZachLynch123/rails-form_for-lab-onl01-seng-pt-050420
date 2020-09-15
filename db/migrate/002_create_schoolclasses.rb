b8b6941dd0c905b587de02f3d783ad1b26ab031f
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-09-15 06:06:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_schoolclasses.rb
+++ db/migrate/002_create_schoolclasses.rb
@@ -1,1 +1,13 @@
-class CreateSchoolClasses < ActiveRecord::Migrate
+class CreateSchoolClasses < ActiveRecord::Migration
+   
+  def change
+    create_table :school_class do |t|
+      t.string :title
+      t.integer :room_number
+
+      t.timestamps null: false
+    end
+    
+  end
+  
+end
